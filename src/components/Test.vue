52397526aa3f0867416d385344bddc75c98f77f6
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-09-07 09:55:09 +0800

    add ref v-for polyfill

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -10,41 +10,25 @@ import {
   unref,
   watch,
   watchEffect,
-} from '@vue/composition-api'
-import { DragService, DragServiceProps } from './DragAndDropService'
+} from "@vue/composition-api";
+import DragService, { DragServiceProps } from "./DragService";
+import DragParentMapping from "./DragParentMapping";
 export default defineComponent({
-  name: 'Test',
+  name: "Test",
   setup() {
-    const dragProps = new DragServiceProps()
+    const dragProps = new DragServiceProps();
     dragProps.onDrop = (e) => {
-      console.log(e)
-    }
-    const dragService = new DragService(dragProps)
-    const dragParentRef = ref<HTMLElement | null>(null)
-    const dropParentRef = ref<HTMLElement | null>(null)
-    const handleChange = (
-      val: [Ref<HTMLElement | null>, Ref<HTMLElement | null>]
-    ) => {
-      if (!val[0].value || !val[1].value) {
-        return
-      }
-      dragService.dragRefList.value = [...(val[0].value.children as any)]
-      dragService.dropRefList.value = [...(val[1].value.children as any)]
-    }
-    watch([dragParentRef, dropParentRef], handleChange)
-    onMounted(() => {
-      handleChange([dragParentRef, dropParentRef])
-    })
-
+      console.log(e);
+    };
+    const dragService = new DragService(dragProps);
     return {
       dragList: [1, 2, 3, 4, 5],
       dropList: [6, 7, 8, 9, 10],
-      dragParentRef,
-      dropParentRef,
+      ...DragParentMapping(dragService.dragRefList, dragService.dropRefList),
       ...dragService,
-    }
+    };
   },
-})
+});
 </script>
 
 <template>
@@ -52,14 +36,14 @@ export default defineComponent({
     <h1>当前拖拽 第「{{ currentDragIndex }}」个</h1>
     <h2>当前放置 第「{{ currentDropIndex }}」个</h2>
     <div ref="dragParentRef">
-      <div v-for="(item, i) in dragList" :key="item">
+      <div v-for="(item) in dragList" :key="item">
         <a-card>
           <a-button>拖拽元素</a-button>
         </a-card>
       </div>
     </div>
     <div ref="dropParentRef">
-      <div v-for="(item, i) in dropList" :key="item">
+      <div v-for="(item) in dropList" :key="item">
         <a-card>
           <a-button>放置区域</a-button>
         </a-card>
